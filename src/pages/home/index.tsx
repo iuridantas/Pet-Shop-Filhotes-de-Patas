ee58832c2c6ba1aedd1bcb9cc66ee26eb900e40e
iuridantas <uri.dantas@hotmail.com>
2023-08-03 15:04:21 -0300

    feat: created sessionTitle

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import { SessionBannersHome } from '../../components/session/sessionHome/sessionBanner';
 import { SessionOfLoveCommitmentAndDedication } from '../../components/session/sessionHome/sessionOfLoveCommitmentAndDedication';
+import { SessionTitle } from '../../components/session/sessionHome/sessionTitle';
 import { Container } from './style';
 
 export function Home() {
   return (
     <Container aria-label="Página inicial">
       <SessionBannersHome/>
+      <SessionTitle/>
       <SessionOfLoveCommitmentAndDedication/>
     </Container>
   );
