2cfd26452eb585f8bc30ebe86be4d84403c8a3b0
iuridantas <uri.dantas@hotmail.com>
2023-08-18 12:14:28 -0300

    feat:sessionHighlights terminated

--- src/components/session/sessionHome/sessionHighlights/index.tsx
+++ src/components/session/sessionHome/sessionHighlights/index.tsx
@@ -1,6 +1,9 @@
+import { useNavigate } from 'react-router-dom';
 import { HighlightsSession } from './style';
 
 export function SessionHighlights() {
+  const navigate = useNavigate();
+
   return (
     <section aria-label="Destaques dos Produtos Oferecidos pelo Pet Shop">
       <HighlightsSession>
@@ -9,22 +12,58 @@ export function SessionHighlights() {
           <ul>
             <li>
               <a href="">
-                <img src="/img/contato.png" alt="" />
+                <img
+                  src="/img/1.png"
+                  alt="Icone representando ração"
+                  onClick={() => {
+                    navigate('/cachorro?category=Ração');
+                  }}
+                />
+                <div>
+                  <p>Rações</p>
+                </div>
               </a>
             </li>
             <li>
               <a href="">
-                <img src="/img/contato.png" alt="" />
+                <img
+                  src="/img/2.png"
+                  alt="Icone representando higiene e limpeza"
+                  onClick={() => {
+                    navigate('/cachorro?category=Beleza%20e%20Limpeza');
+                  }}
+                />
+                <div>
+                  <p>Higiene e limpeza</p>
+                </div>
               </a>
             </li>
             <li>
               <a href="">
-                <img src="/img/contato.png" alt="" />
+                <img
+                  src="/img/3.png"
+                  alt="Icone representando farmácia"
+                  onClick={() => {
+                    navigate('/cachorro?category=Farmácia');
+                  }}
+                />
+                <div>
+                  <p>Farmácia</p>
+                </div>
               </a>
             </li>
             <li>
               <a href="">
-                <img src="/img/contato.png" alt="" />
+                <img
+                  src="/img/4.png"
+                  alt="Icone representando acessórios de alimentação"
+                  onClick={() => {
+                    navigate('/cachorro?category=Comedouros%20e%20Bebedouros');
+                  }}
+                />
+                <div>
+                  <p>Acessórios de alimentação</p>
+                </div>
               </a>
             </li>
           </ul>
@@ -34,22 +73,58 @@ export function SessionHighlights() {
           <ul>
             <li>
               <a href="">
-                <img src="/img/contato.png" alt="" />
+                <img
+                  src="/img/5.png"
+                  alt="Icone representando brinquedos"
+                  onClick={() => {
+                    navigate('/gato?category=Brinquedos');
+                  }}
+                />
+                <div>
+                  <p>Brinquedos</p>
+                </div>
               </a>
             </li>
             <li>
               <a href="">
-                <img src="/img/contato.png" alt="" />
+                <img
+                  src="/img/2.png"
+                  alt="Icone representando higiene e limpeza"
+                  onClick={() => {
+                    navigate('/gato?category=Beleza%20e%20Limpeza');
+                  }}
+                />
+                <div>
+                  <p>Higiene e limpeza</p>
+                </div>
               </a>
             </li>
             <li>
               <a href="">
-                <img src="/img/contato.png" alt="" />
+                <img
+                  src="/img/3.png"
+                  alt="Icone representando farmácia"
+                  onClick={() => {
+                    navigate('/gato?category=Farmácia');
+                  }}
+                />
+                <div>
+                  <p>Farmácia</p>
+                </div>
               </a>
             </li>
             <li>
               <a href="">
-                <img src="/img/contato.png" alt="" />
+                <img
+                  src="/img/4.png"
+                  alt="Icone representando acessórios de alimentação"
+                  onClick={() => {
+                    navigate('/gato?category=Comedouros%20e%20Bebedouros');
+                  }}
+                />
+                <div>
+                  <p>Acessórios de alimentação</p>
+                </div>
               </a>
             </li>
           </ul>
--- src/components/session/sessionHome/sessionHighlights/style.ts
+++ src/components/session/sessionHome/sessionHighlights/style.ts
@@ -25,14 +25,44 @@ export const HighlightsSession = styled.div`
 
   a {
     display: flex;
+    flex-direction: column;
     padding: 0 7px;
+    text-align: center;
+    cursor: pointer;
+    text-decoration: none;
+    color: #000000;
+  }
+
+  p {
+    width: 105px;
+    font-size: 20px;
+    font-weight: bold;
   }
 
   img {
     width: 150px;
     height: 150px;
+    border-radius: 80px;
   }
 
   @media (max-width: 600px) {
+    display: flex;
+    flex-direction: column;
+    margin: 5px auto;
+
+    h2 {
+      font-size: 20px;
+    }
+
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      margin: 10px auto;
+    }
+
+    li {
+      margin: 10px auto;
+    }
   }
 `;
